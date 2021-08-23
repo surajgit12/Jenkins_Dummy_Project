dab8414827e8815d0afdf6b5e11352a9722c7bf3
GlobalLogic <GlobalLogic@NAG1-LHP-N07423.synapse.com>
2021-08-23 19:45:58 +0530

    Added a Multiply Constructor

--- src/test/java/Test1.java
+++ src/test/java/Test1.java
@@ -6,9 +6,14 @@ public class Test1 {
 		System.out.println("Hello Word");
 	}
 	
+	Test1(int a ,int b){
+	
+		System.out.println("Multiple--->"+ a*b);
+	}
 	public static void main(String[] args) {
 		
 		new Test1();
+		new Test1(10,5);
 
 
 	}
